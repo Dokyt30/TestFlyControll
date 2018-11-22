5822fd18917ab4c5dff118bc9bee7606ec1d3fb4
y <y>
2018-11-22 16:01:16 +0900

    flight controll test

---
+++ Source/TestFlyControll/TestFlyControll.cpp
@@ -0,0 +1,8 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "TestFlyControll.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, TestFlyControll, "TestFlyControll");
+
+DEFINE_LOG_CATEGORY(LogFlying)
